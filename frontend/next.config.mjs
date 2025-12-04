ec352f2c6649c2be0c8295138b755e7eca970823
ShenLongVansh <shenlong.codes@gmail.com>
2025-12-04 23:39:17 +0530

    added re routing to the /dashboard/today for niche case situation

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard/today",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
